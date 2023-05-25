41dad6ba00f28169056cd1f00922e0782fe900d9
hattima-tim <irfanridwan20@gmail.com>
2023-05-25 08:27:52 +0000

    add serialize and deserialize user functions

--- app.js
+++ app.js
@@ -41,6 +41,19 @@ passport.use(
   })
 );
 
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
